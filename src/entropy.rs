0d11ab147ebd4ba8b0a153403e2c6f2d4077cee8
maksymdidukh <maxym.didukh@logivations.com>
2024-02-18 17:41:53 +0200

    remove useless comments

--- src/entropy.rs
+++ src/entropy.rs
@@ -4,37 +4,7 @@ use std::io::{self, BufRead, BufReader, Write};
 use std::path::Path;
 
 mod utils;
-use crate::utils::{preprocess_text, remove_spaces}; // Adjust function_name1, function_name2 accordingly
-
-
-// fn is_cyrillic(c: &char) -> bool {
-//     (*c as u32) >= 0x0400 && (*c as u32) <= 0x04FF
-// }
-
-// fn preprocess_text(text: &str) -> Option<String> {
-//     let mut processed_text = String::new();
-    
-//     for c in text.chars() {
-//         if c.is_alphabetic() && is_cyrillic(&c) {
-//             processed_text.push(c.to_lowercase().next().unwrap());
-//         } else if c == ' ' {
-//             if processed_text.ends_with(' ') {
-//                 continue;
-//             }
-//             processed_text.push(c);
-//         }
-//     }
-
-//     if processed_text.is_empty() {
-//         None
-//     } else {
-//         Some(processed_text)
-//     }
-// }
-
-// fn remove_spaces(text: &str) -> String {
-//     text.chars().filter(|&c| c != ' ').collect()
-// }
+use crate::utils::{preprocess_text, remove_spaces, is_cyrillic}; 
 
 fn get_letter_frequency(text: &str) -> HashMap<char, i64> {
     let mut frequencies: HashMap<char, i64> = HashMap::new();
