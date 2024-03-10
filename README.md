06e11ec58d9345b01188a56aa01e8260abd805d7
maksymdidukh <maxym.didukh@logivations.com>
2024-03-10 21:15:58 +0200

    make alphabet and probabilities global

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 to do: 
 - proper readme 
 - setup cargo
+
+before defending lab2:
+- make all posible local variables global (alphabet, probailies, etc.)
--- src/vigenere_decode.rs
+++ src/vigenere_decode.rs
@@ -1,12 +1,20 @@
-use std::fs::File;
-use std::io::{self, BufRead, BufReader, Write};
-use std::path::Path;
 use std::fs;
+use fs::File;
+use std::io::{self, Write};
+use std::path::Path;
+use std::collections::HashMap;
 
 mod utils;
-use utils::process_file;
-use utils::coincidence;
-use std::collections::HashMap;
+use utils::{process_file, coincidence};
+
+const ALPHABET: &str = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const PROBABILITIES: [f64; 33] = [
+    0.08143, 0.01667, 0.04604, 0.01632, 0.03084, 0.08027, 0.00150, 0.00884,
+    0.01507, 0.07563, 0.01200, 0.03374, 0.03952, 0.03270, 0.06503, 0.11143,
+    0.02931, 0.04774, 0.05482, 0.06829, 0.02647, 0.00310, 0.00827, 0.00455,
+    0.01458, 0.00681, 0.00330, 0.01808, 0.01752, 0.00425, 0.00735, 0.01818,
+    0.00036,
+];
 
 fn divide_into_blocks(text: &str, r: usize) -> Vec<String> {
     let mut blocks = Vec::new();
@@ -19,9 +27,8 @@ fn divide_into_blocks(text: &str, r: usize) -> Vec<String> {
     blocks
 }
 
-fn compute_r(processed_text: &str, alphabet: &str) -> Option<usize> {
-    let alphabet_len = alphabet.len();
-    let i_input = coincidence(processed_text, alphabet);
+fn compute_r(processed_text: &str) -> Option<usize> {
+    let i_input = coincidence(processed_text, ALPHABET);
     println!("I for input text (message): {}", i_input);
 
     let mut closest_r: Option<usize> = None;
@@ -32,7 +39,7 @@ fn compute_r(processed_text: &str, alphabet: &str) -> Option<usize> {
 
         let mut blocks_coincidence: f64 = 0.0;
         for block in &blocks {
-            blocks_coincidence += coincidence(block, alphabet);
+            blocks_coincidence += coincidence(block, ALPHABET);
         }
 
         let average_coincidence = blocks_coincidence / blocks.len() as f64;
@@ -49,12 +56,11 @@ fn compute_r(processed_text: &str, alphabet: &str) -> Option<usize> {
 }
 
 fn main() -> io::Result<()> {
-    let alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     let input_file = "../text_files/vigenere_cipher/to_decode/input.txt";
     let preprocessed_file = "../text_files/vigenere_cipher/to_decode/preprocessed.txt";
     let processed_text = process_file(input_file, preprocessed_file, false)?;
 
-    if let Some(r) = compute_r(&processed_text, alphabet) {
+    if let Some(r) = compute_r(&processed_text) {
         println!("Closest block size to i_m: {}", r);
     }
 
